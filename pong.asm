5bb830dc99f46a223f7309e46b464bdab5534bad
José Guilherme <145349759+zeguilherme1@users.noreply.github.com>
2025-12-12 07:57:32 -0500

    FIX: controle title at tutorial screen

--- pong.asm
+++ pong.asm
@@ -1,26 +1,3 @@
-	; - - - - - - - TABELA DE CORES - - - - - - - 
-	; adicione ao caracter para Selecionar a cor correspondente
-	
-	; 0 branco 0000 0000
-	; 256 marrom 0001 0000
-	; 512 verde 0010 0000
-	; 768 oliva 0011 0000
-	; 1024 azul marinho 0100 0000
-	; 1280 roxo 0101 0000
-	; 1536 teal 0110 0000
-	; 1792 prata 0111 0000
-	; 2048 cinza 1000 0000
-	; 2304 vermelho 1001 0000
-	; 512 lima 1010 0000
-	; 2816 amarelo 1011 0000
-	; 3072 azul 1100 0000
-	; 3328 rosa 1101 0000
-	; 3584 aqua 1110 0000
-	; 3840 branco 1111 0000
-
-
-
-
 reset:
 	
 	
@@ -31,7 +8,7 @@ plank_vit: string "VITORIA DO PLANKTON!"
 msg_pausa: string "JOGO PAUSADO"
 msg_limpa_pausa: string "            "  ; Espaços vazios para apagar a mensagem
 str_tut_btn: string "TUTORIAL: APERTE T"
-str_tit_tut: string "---------- CONTROLES ----------"
+str_tit_tut: string "----- CONTROLES -----"
 str_tut_bob: string "BOB (ESQ):   W (SOBE)  S (DESCE)"
 str_tut_plk: string "PLANK (DIR): I (SOBE)  J (DESCE)"
 str_tut_pau: string "PAUSA: P"
